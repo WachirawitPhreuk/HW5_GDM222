e55d6e36da503ef6f224c8674644421478945f37
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2023-05-18 21:51:09 +0700

    Q3 V.1

---
+++ Q3/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program {
+    static void Main (string[] args) {
+        int m = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        
+        char[] alphabetCode = new char[m];
+        int[] numberCode = new int[n];
+
+        for(int i = 0; i<m ;i++) {
+            alphabetCode[i] = 'A';
+        }
+
+        for(int i = 0; i<n ;i++) {
+            numberCode[i] = 0;
+        }
+
+       Console.WriteLine("{0}", ); 
+        /*while(true) {
+        string items =  Console.ReadLine();
+        if(items == "Stop") {
+            break;
+        }
+        }
+
+        string itemsCode = Console.ReadLine();*/
+    }
+
+    static int Numbers(int num, int now) {
+        if(now == 0) {
+        
+        if(num < 9) {num += 1;}
+        
+        else if(num == 9) {num = 0;}
+        }
+        return num;
+    }
+    /*static string Items(string items) {
+
+    }*/
+}
