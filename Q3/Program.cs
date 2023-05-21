a8fb71a3b16bc78cc61808b30fea28c272be43d3
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2023-05-21 14:12:00 +0700

    Update Program.cs

--- Q3/Program.cs
+++ Q3/Program.cs
@@ -3,18 +3,31 @@
         int alphabet = int.Parse(Console.ReadLine());
         int digits = int.Parse(Console.ReadLine());
         
+        int count = 0;
         string lastCode = new string('A', alphabet) + new string('0', digits);
         int maxProduct  = (alphabet*26) + (digits*10);
-        string items = Console.ReadLine();
         
-        /*while(true) {
-        string items =  Console.ReadLine();
+        string items = Console.ReadLine();
+        IDictionary<string, string> itemsID = new Dictionary<string, string>();
+        itemsID.Add(lastCode, items);
+
+        while(true) {
+        items =  Console.ReadLine();
+        
         if(items == "Stop") {
             break;
         }
+
+        if(count >= maxProduct) {
+        count = 0;
+        lastCode = new string('A', alphabet) + new string('0', digits);
+        maxProduct  = (alphabet*26) + (digits*10);
+        continue;
         }
 
-        string itemsCode = Console.ReadLine();*/
+        }
+
+        string itemsCode = Console.ReadLine();
     }
 
     static int Numbers(int num, int now) {
